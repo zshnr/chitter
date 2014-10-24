eba7076689183f52c47b3dd07237accd648ffb95
Zeeshan Rasool <zrasool88@gmail.com>
2014-10-24 16:07:09 +0100

    i'm green now. little mistakes fixed. peep is being posted to the page and is visible

--- app/chitter.rb
+++ app/chitter.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 require 'data_mapper' 
 
-env = ENV["RACK_ENV"] || "development"
+env = ENV["RACK_ENV"] || "developmenta"
 
 DataMapper.setup(:default, "postgres://localhost/chitter_#{env}")
 
